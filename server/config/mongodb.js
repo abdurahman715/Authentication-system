9e0d3831bc6bc2c7deb00220da94dbf15d77bfa1
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-16 11:27:49 +0530

    Add mongodb connetion configuration

---
+++ server/config/mongodb.js
@@ -0,0 +1,6 @@
+import mongoose, { mongo } from "mongoose";
+const connectdb = async () => {
+  mongoose.connection.on("connected", () => console.log("Database connected"));
+  await mongoose.connect(`${process.env.MONGODB_URL}/mern-auth`);
+};
+export default connectdb;
