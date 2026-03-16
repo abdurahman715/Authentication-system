31a49417e25edf54878100f5d890adba753add11
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-16 13:44:17 +0530

    Implement user logout functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -61,3 +61,17 @@ export const login = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+//Logout
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+    return res.json({ success: true, message: "Logged out" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
