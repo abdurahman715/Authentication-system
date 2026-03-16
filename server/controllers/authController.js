79e9f65884756f81b1084b4336248ace54a78d4e
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-16 15:39:54 +0530

    Implement welcome email functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import transporter from "../config/nodeMailer.js";
 //Registration
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -24,6 +25,14 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+    //Sending welcome email
+    const mailOptions = {
+      from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to AuthSystem",
+      text: `Welcome to auth website.Your account has been created with email id ${email}`,
+    };
+    await transporter.sendMail(mailOptions);
     return res.json({ success: true, message: "Registration successful" });
   } catch (error) {
     return res.json({ success: false, message: error.message });
