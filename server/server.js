d44980a07436d57de6fb2bd83395193060069439
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-16 11:17:23 +0530

    created first testing route

--- server/server.js
+++ server/server.js
@@ -7,4 +7,7 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 app.listen(port, () => console.log(`Server started on Port ${port}`));
