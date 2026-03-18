32c140256903ec78d971f18da53eebb7e67d235c
Muhammed Abdurahman C K <abdurahmanck715@gmail.com>
2026-03-18 10:12:28 +0530

    fixed bugs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,7 @@
-import userModel from "../models/userModel";
+import userModel from "../models/userModel.js";
 export const getUserData = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req;
     const user = await userModel.findById(userId);
     if (!user) {
       return res.json({ success: false, message: "User not found" });
